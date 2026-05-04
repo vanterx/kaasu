fcb335a2e618d76b9ffe3e2bc80ebded726b6f37
vanterx <tsx113@gmail.com>
2026-05-04 23:50:36 +1200

    chore: bump versionName to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = (System.currentTimeMillis() / 1000).toInt()
-        versionName = "1.2.0"
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
